a1de9e1e3628c5d192f1ed222922a67205e877f0
ashankdavid <ashank2507david@gmail.com>
2026-09-21 19:32:32 +0530

    Frontend cleaned and stabilized

--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,3 @@
-// function App() {
-//   return (
-//     <div>
-//       <h1>Hello Firends</h1>
-//       <p>Chai Peelo</p>
-//     </div>
-//   );
-// }
-
-// export default App;
-// import "./App.css";
-// function App(){
-//   const tarinerName="Nirmal";
-//   const trainingDay=3;
-//   return(
-//     <main className="app">
-//       <section className="welcome-card">
-//       <p className="day-label">React Day {trainingDay}</p>
-//       <h1 className="title">College Course Explorer </h1>
-//       <p>Trainer:{tarinerName}</p>
-//       <p>React Learning Project</p>
-//       <p>Used React,JSX,componets,CSS</p>
-//       </section>   
-//     </main>       
-//   );
-// }
-// export default App;
-// import "./App.css";
-// import Navbar from "./components/Navbar";
-// import Hero from"./components/Hero";
-// import EventSection from "./components/EventSection";
-// import Footer from "./components/Footer";
-// function App(){
-//   return(
-//     <div>
-//       <Navbar />
-//       <main id="home">    
-//       <Hero />
-//       <EventSection />
-//       </main> 
-//       <Footer /> 
-//     </div>
-//   );
-// }
-// export default App;
-// import { useState } from "react";
-// import "./App.css";
-// import Navbar from "./components/Navbar";
-// import Hero from "./components/Hero";
-// import EventForm from "./components/EventForm";
-// import EventSection from "./components/EventSection";
-// import Footer from "./components/Footer";
-// import { initialEvents } from "./data/events";
-
-// function App() {
-//   const [events, setEvents] = useState(initialEvents);
-
-//   function handleAddEvent(newEvent) {
-//     setEvents([...events, newEvent]);
-//   }
-
-//   return (
-//     <div>
-//       <Navbar />
-
-//       <main id="home">
-//         <Hero
-//           title="Discover What Is Happening on Campus"
-//           description="Find workshops, sports activities, club meetings, and opportunities to connect with other students."
-//         />
-
-//         <EventForm onAddEvent={handleAddEvent} />
-
-//         <EventSection events={events} />
-//       </main>
-
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// export default App;
 import { useState } from "react";
 import { Routes, Route } from "react-router";
 
@@ -96,65 +14,64 @@ import AboutPage from "./pages/AboutPage";
 import { initialEvents } from "./data/events";
 
 function App() {
-  const [events, setEvents] = useState(initialEvents);
+    const [events, setEvents] = useState(initialEvents);
 
-  function handleAddEvent(newEvent) {
-    setEvents([...events, newEvent]);
-  }
+    function handleAddEvent(newEvent) {
+        setEvents([...events, newEvent]);
+    }
 
-  function handleDeleteEvent(eventId) {
-    const updatedEvents = 
-    events.filter(function (event) {
-      return event.id !== eventId;
-    });
+    function handleDeleteEvent(eventId) {
+        const updatedEvents = events.filter(function (event) {
+            return event.id !== eventId;
+        });
 
-    setEvents(updatedEvents);
-  }
+        setEvents(updatedEvents);
+    }
 
-  return (
-    <div>
-      <Navbar />
+    return (
+        <div>
+            <Navbar />
 
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <HomePage
-              events={events}
-              onAddEvent={handleAddEvent}
-              onDeleteEvent={handleDeleteEvent}
-            />
-          }
-        />
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <HomePage
+                            events={events}
+                            onAddEvent={handleAddEvent}
+                            onDeleteEvent={handleDeleteEvent}
+                        />
+                    }
+                />
 
-        <Route
-          path="/events"
-          element={
-            <EventsPage
-              events={events}
-              onDeleteEvent={handleDeleteEvent}
-            />
-          }
-        />
+                <Route
+                    path="/events"
+                    element={
+                        <EventsPage
+                            events={events}
+                            onDeleteEvent={handleDeleteEvent}
+                        />
+                    }
+                />
 
-        <Route
-          path="/events/:eventId"
-          element={
-            <EventDetailsPage
-              events={events}
-            />
-          }
-        />
+                <Route
+                    path="/events/:eventId"
+                    element={
+                        <EventDetailsPage
+                            events={events}
+                        />
+                    }
+                />
 
-        <Route
-          path="/about"
-          element={<AboutPage />}
-        />
-      </Routes>
+                <Route
+                    path="/about"
+                    element={<AboutPage />}
+                />
+            </Routes>
 
-      <Footer />
-    </div>
-  );
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,26 @@
-function Hero(){
-    return(
+import { Link } from "react-router";
+
+function Hero({ title, description }) {
+    return (
         <section className="hero">
-            <p className="hero-label">College Events and Clubs</p>
-            <h1>Discover what is Happening?</h1>
-            <p className="hero-description">
-                Find Workshops,sports activities,club meeting 
+            <p className="hero-label">
+                College Events and Clubs
             </p>
-            <a className="hero-button" href="#events">Explore Events</a>
+
+            <h1>{title}</h1>
+
+            <p className="hero-description">
+                {description}
+            </p>
+
+            <Link
+                className="hero-button"
+                to="/events"
+            >
+                Explore Events
+            </Link>
         </section>
     );
 }
+
 export default Hero;
